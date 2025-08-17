557c6899e804ca68e633e2e30c78499756c86b9c
vikito11111 <vikito11111@gmail.com>
2025-08-17 13:33:42 +0300

    Update SearchResults.jsx

--- src/components/layout/mobile/SearchResults.jsx
+++ src/components/layout/mobile/SearchResults.jsx
@@ -11,7 +11,7 @@ const SearchResults = ({ results, onCityClick, onLocationClick, onExitSearch }) 
 
 return (
         <>
-            <div className="fixed inset-0 top-[50px] bg-white z-30 flex flex-col">
+            <div className="absolute inset-0 top-[68px] bg-white z-30 flex flex-col">
                 <div className="flex-grow overflow-y-auto">
                     {cities.length > 0 && (
                         <div className="p-4">
