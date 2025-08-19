c85b2213b2c8c7be94f73a28cbc23e7c3829b949
vikito11111 <vikito11111@gmail.com>
2025-08-19 12:55:35 +0300

    Added Padding For Share Modal
    - Added small padding for the MobileShareModaljsx

--- src/components/layout/mobile/MobileShareModal.jsx
+++ src/components/layout/mobile/MobileShareModal.jsx
@@ -83,7 +83,7 @@ const MobileShareModal = ({ isOpen, onClose, place }) => {
 
     return (
         <div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4"
             onClick={onClose}
         >
             <div
